06a96bbf743cf8f15e93c10de4f90b52ccd2f143
Dwmla <damlagencer322@gmail.com>
2021-05-08 23:03:39 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,269 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} bu cehenneme geldi ve artk organlarini çalabilrz!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+  if (msg.content.toLowerCase() === '?ayse') {
+    msg.reply('h efmdm');
+  }
+  if (msg.content.toLowerCase() === '?naber') {
+    msg.reply('h ii sen');
+  }
+  if (msg.content.toLowerCase() === '?tm özür dilerm') {
+    msg.reply('***tm kabl edm ama bida ypma.***');
+  }
+  if (msg.content.toLowerCase() === '?özr dilerwm') {
+    msg.reply('***tm kabl edm ama bida ypma.***');
+  }
+  if (msg.content.toLowerCase() === '?özr dlerwm') {
+    msg.reply('***tm kabl edm ama bida ypma.***');
+  }
+  if (msg.content.toLowerCase() === '?aşkım') {
+    msg.reply('***siktr git ay sen benim seviyemde dglsn***');
+  }
+  if (msg.content.toLowerCase() === '?severm') {
+    msg.reply('***tm smdi arabama bn***');
+  }
+  if (msg.content.toLowerCase() === '?yo') {
+    msg.reply('***bin dedm***');
+  }
+  if (msg.content.toLowerCase() === '?sevgili olalım mı') {
+    msg.reply('***h***');
+  }
+  if (msg.content.toLowerCase() === '?sewgili olalım mı') {
+    msg.reply('***h***');
+  }
+  if (msg.content.toLowerCase() === '?sewgili olalwm mı') {
+    msg.reply('***h***');
+  }
+  if (msg.content.toLowerCase() === '?sewgli olalwm m') {
+    msg.reply('***h***');
+  }
+  if (msg.content.toLowerCase() === '?sewgli olalwm mw') {
+    msg.reply('***h***');
+  }
+  if (msg.content.toLowerCase() === '?özür dilerim') {
+    msg.reply('***tm kabl edm ama bida ypma.***');
+  }
+  if (msg.content.toLowerCase() === '?mısırlı balık kraker severim') {
+    msg.reply('o zaman arabama bin :)');
+  }
+  if (msg.content.toLowerCase() === '?hitler') {
+    msg.reply('https://cdn.discordapp.com/attachments/792860285933060126/814540316685893632/HTLR.PNG');
+  }
+  if (msg.content.toLowerCase() === '?gum') {
+    msg.reply('https://www.youtube.com/watch?v=fQzLNELHSBw&ab_channel=SauceLord69');
+  }
+  if (msg.content.toLowerCase() === '?ah') {
+    msg.reply('https://www.youtube.com/watch?v=1PwWkdkDHAo&ab_channel=BENBROS');
+  }
+  if (msg.content.toLowerCase() === '?ayse malsın') {
+    msg.reply('no u');
+  }
+  if (msg.content.toLowerCase() === '?ayse ben mal mıyım') {
+    msg.reply('mal dglsin ama beynnde sorun war.');
+  }
+  if (msg.content.toLowerCase() === '?ayse hentai') {
+    msg.reply('https://www.reddit.com/r/hentai/');
+  }
+  if (msg.content.toLowerCase() === '?h') {
+    msg.reply('h');
+  }
+  if (msg.content.toLowerCase() === '?merhaba') {
+    msg.reply('h');
+  }
+  if (msg.content.toLowerCase() === '?bende iyi') {
+    msg.reply('misrli balk krker sewer misn');
+  }
+  if (msg.content.toLowerCase() === '?hayır') {
+    msg.reply('mrk etme seni kacirmicam.');
+  }
+  if (msg.content.toLowerCase() === '?evet') {
+    msg.reply('o zaman arabama bin :)');
+  }
+  if (msg.content.toLowerCase() === '?tamam') {
+    msg.reply('tm smdi arabama bin yksa gözlerini oyarm.');
+  }
+  if (msg.content.toLowerCase() === '?banane') {
+    msg.reply('get in the car or i have to fuck you.');
+  }
+  if (msg.content.toLowerCase() === '?uwu') {
+    msg.reply('OwO');
+  }
+  if (msg.content.toLowerCase() === '?owo') {
+    msg.reply('UwU');
+  }
+  if (msg.content.toLowerCase() === '?selam') {
+    msg.reply('mrb');
+  }
+  if (msg.content.toLowerCase() === '?ii') {
+    msg.reply('misrli balk krker sewer misn');
+  }
+  if (msg.content.toLowerCase() === '?evt') {
+    msg.reply('o zaman arabama bin :)');
+  }
+  if (msg.content.toLowerCase() === '?ewt') {
+    msg.reply('o zaman arabama bin :)');
+  }
+  if (msg.content.toLowerCase() === '?hyr') {
+    msg.reply('tm simdi arabama bin yoksa seni skrm.');
+  }
+  if (msg.content.toLowerCase() === '?naber lan') {
+    msg.reply('h ii sen');
+  }
+  if (msg.content.toLowerCase() === '?kötü') {
+    msg.reply('nedn');
+  }
+  if (msg.content.toLowerCase() === '?sanane') {
+    msg.reply('mad aise noises*');
+  }
+  if (msg.content.toLowerCase() === '?afk') {
+    msg.reply('ok,                                                                                                                                                                                                                                                                                                                                                   Kullanıcı artık afk :(');
+  }
+  if (msg.content.toLowerCase() === '?geldim') {
+    msg.reply('hg');
+  }
+  if (msg.content.toLowerCase() === 'geldm') {
+    msg.reply('hg');
+  }
+  if (msg.content.toLowerCase() === '?tm') {
+    msg.reply('SEN BENM KOCM MISIN');
+  }
+  if (msg.content.toLowerCase() === '?hayir') {
+    msg.reply('***merak etme seni kacirmicam***');
+  }
+  if (msg.content.toLowerCase() === '?hayr') {
+    msg.reply('***merak etme seni kacirmicam***');
+  }
+  if (msg.content.toLowerCase() === '?hyr') {
+    msg.reply('***merak etme seni kacirmicam***');
+  }
+  if (msg.content.toLowerCase() === '?peki') {
+    msg.reply('***tm simdi arabama bin***');
+  }
+  if (msg.content.toLowerCase() === '?hb') {
+    msg.reply('nasılsn');
+  }
+  if (msg.content.toLowerCase() === '?ii sen') {
+    msg.reply('bende iyi');
+  }
+  if (msg.content.toLowerCase() === '?iyi sen') {
+    msg.reply('bende iyi');
+  }
+  if (msg.content.toLowerCase() === '?bindim') {
+    msg.reply('kesinlikle cocuk kacirmiorm');
+  }
+  if (msg.content.toLowerCase() === '?bindm') {
+    msg.reply('kesinlikle cocuk kacirmiorm');
+  }
+  if (msg.content.toLowerCase() === '?kaçır') {
+    msg.reply('kacirirr cutely');
+  }
+  if (msg.content.toLowerCase() === '?kacir') {
+    msg.reply('kacirirr cutely');
+  }
+  if (msg.content.toLowerCase() === '?kaçir') {
+    msg.reply('kacirirr cutely');
+  }
+  if (msg.content.toLowerCase() === '?kaçırdın') {
+    msg.reply('tm smdi burda acliktan öL.');
+  }
+  if (msg.content.toLowerCase() === '?kacirdin') {
+    msg.reply('tm smdi burda acliktan öL.');
+  }
+  if (msg.content.toLowerCase() === '?kaçrdın') {
+    msg.reply('tm smdi burda acliktan öL.');
+  }
+  if (msg.content.toLowerCase() === '?tanışalım mı') {
+    msg.reply('bnm kocam wra');
+  }
+  if (msg.content.toLowerCase() === '?tanışalm mı') {
+    msg.reply('bnm kocam wra');
+  }
+  if (msg.content.toLowerCase() === '?tanısalwm mı') {
+    msg.reply('bnm kocam wra');
+  }
+  if (msg.content.toLowerCase() === '?naber ln') {
+    msg.reply('n');
+  }
+  if (msg.content.toLowerCase() === '?napim') {
+    msg.reply('bok ye');
+  }
+  if (msg.content.toLowerCase() === '?happy thoughts') {
+    msg.reply('https://www.youtube.com/watch?v=OBWhueroFTE');
+  }
+  if (msg.content.toLowerCase() === 'bota noldu') {
+    msg.reply('ebenin amı oldu');
+  }
+  if (msg.content.toLowerCase() === 'bota ne oldu') {
+    msg.reply('ebenin amı oldu');
+  }
+  if (msg.content.toLowerCase() === 'bota noldu lan') {
+    msg.reply('***ebenin amı oldu***');
+  }
+  if (msg.content.toLowerCase() === 'bota noldu ln') {
+    msg.reply('***ebenin amı oldu***');
+  }
+  if (msg.content.toLowerCase() === 'bota noldu aq') {
+    msg.reply('***ebenin amı oldu***');
+  }
+  if (msg.content.toLowerCase() === 'bota noldu aq') {
+    msg.reply('***ebenin amı oldu***');
+  }
+  if (msg.content.toLowerCase() === '?sni sewiyorm') {
+    msg.reply('***nasil bir beyin hasari aldn we beni sewmeye krr wrdn blmiorm ama ltfn sktr.***');
+  }
+  if (msg.content.toLowerCase() === '?seni sewiyorm') {
+    msg.reply('***nasil bir beyin hasari aldn we beni sewmeye krr wrdn blmiorm ama ltfn sktr.***');
+  }
+  if (msg.content.toLowerCase() === '?sevdim seni') {
+    msg.reply('***nasil bir beyin hasari aldn we beni sewmeye krr wrdn blmiorm ama ltfn sktr.***');
+  }
+  if (msg.content.toLowerCase() === '?sus') {
+    msg.reply('***sen sus***');
+  }
+  if (msg.content.toLowerCase() === '?aptal') {
+    msg.reply('***sensn o***');
+  }
+  if (msg.content.toLowerCase() === '?misirli balık krker ister misin') {
+    msg.reply('a olur');
+  }
+  if (msg.content.toLowerCase() === '?ayse aptalsın') {
+    msg.reply('***sensn o***');
+  }
+  if (msg.content.toLowerCase() === '?gerizekalı') {
+    msg.reply('***insann kndni blmesi gzl birsy***');
+  }
+  if (msg.content.toLowerCase() === '?özür dilerim ayse') {
+    msg.reply('***tm kabl edm ama bida ypma.***');
+  }
+  if (msg.content.toLowerCase() === '?evt benim') {
+    msg.reply('***insann kndni blmesi gzl birsy***');
+  }
+  if (msg.content.toLowerCase() === '?evet benim') {
+    msg.reply('***insann kndni blmesi gzl birsy***');
+  }
+  if (msg.content.toLowerCase() === '?ewt benm') {
+    msg.reply('***insann kndni blmesi gzl birsy***');
+  }
+  if (msg.content.toLowerCase() === '?mal') {
+    msg.reply('***snin kadar olamam***');
+  }
+  if (msg.content.toLowerCase() === '?sewdim sni') {
+    msg.reply('***nasil bir beyin hasari aldn we beni sewmeye krr wrdn blmiorm ama ltfn sktr.***');
+  }
+  if (msg.content.toLowerCase() === '?aptalsın') {
+    msg.reply('***sensn o***');
+  }
+  if (msg.content.toLowerCase() === '?seni seviyorum') {
+    msg.reply('***nasil bir beyin hasari aldn we beni sewmeye krr wrdn blmiorm ama ltfn sktr.***');
+  }
+});
+
+client.login('ODM3NjY5MjQ5Mzk2Mzc1NTUy.YIv6SA.2tfhOg-Pntrx7nTISsvvGZ4jFDQ');
